16e3ae15eb6802d0de63ce63a96a0e2e40c6c34c
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-05 23:52:53 +0200

    Harden course validation and aliases

--- component/admin/src/Table/CourseTable.php
+++ component/admin/src/Table/CourseTable.php
@@ -3,11 +3,11 @@ namespace Xdecaro\Component\Decarocourses\Administrator\Table;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\ApplicationHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Table\Table;
-use Joomla\CMS\Uri\Uri;
 use Joomla\Database\DatabaseDriver;
-use Joomla\String\StringHelper;
+use Joomla\Database\ParameterType;
 
 class CourseTable extends Table
 {
@@ -20,21 +20,72 @@ class CourseTable extends Table
     {
         $this->title = trim((string) $this->title);
         $this->code = trim((string) $this->code);
+        $this->description = trim((string) $this->description);
+        $this->ordering = (int) $this->ordering;
+        $this->state = (int) $this->state;
 
         if ($this->title === '') {
             $this->setError('Il titolo del corso è obbligatorio.');
             return false;
         }
 
-        if (trim((string) $this->alias) === '') {
-            $this->alias = $this->title;
+        if (mb_strlen($this->title) > 255) {
+            $this->setError('Il titolo del corso è troppo lungo.');
+            return false;
         }
 
-        $this->alias = StringHelper::strtolower(trim((string) $this->alias));
-        $this->alias = preg_replace('/[^a-z0-9\-]+/u', '-', $this->alias) ?: 'corso-' . time();
-        $this->alias = trim($this->alias, '-');
+        if (mb_strlen($this->code) > 80) {
+            $this->setError('Il codice del corso è troppo lungo.');
+            return false;
+        }
+
+        if (!in_array($this->state, [-2, 0, 1], true)) {
+            $this->state = 0;
+        }
+
+        $sourceAlias = trim((string) $this->alias);
+        $baseAlias = ApplicationHelper::stringURLSafe($sourceAlias !== '' ? $sourceAlias : $this->title);
+
+        if ($baseAlias === '') {
+            $baseAlias = 'corso';
+        }
+
+        $this->alias = $this->getUniqueAlias($baseAlias);
 
         return true;
+    }
+
+    private function getUniqueAlias(string $baseAlias): string
+    {
+        $db = $this->getDbo();
+        $id = (int) $this->id;
+        $pattern = $baseAlias . '-%';
+
+        $query = $db->getQuery(true)
+            ->select($db->quoteName('alias'))
+            ->from($db->quoteName('#__decarocourses_courses'))
+            ->where(
+                '(' . $db->quoteName('alias') . ' = :baseAlias'
+                . ' OR ' . $db->quoteName('alias') . ' LIKE :aliasPattern)'
+            )
+            ->where($db->quoteName('id') . ' <> :currentId')
+            ->bind(':baseAlias', $baseAlias)
+            ->bind(':aliasPattern', $pattern)
+            ->bind(':currentId', $id, ParameterType::INTEGER);
+
+        $aliases = array_fill_keys($db->setQuery($query)->loadColumn(), true);
+
+        if (!isset($aliases[$baseAlias])) {
+            return $baseAlias;
+        }
+
+        $suffix = 2;
+
+        while (isset($aliases[$baseAlias . '-' . $suffix])) {
+            ++$suffix;
+        }
+
+        return $baseAlias . '-' . $suffix;
     }
 
     public function store($updateNulls = true): bool
