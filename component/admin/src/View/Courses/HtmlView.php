2f926e788160fed8b226794df7df21fab85ca9c8
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-05 23:53:03 +0200

    Improve courses toolbar and permissions

--- component/admin/src/View/Courses/HtmlView.php
+++ component/admin/src/View/Courses/HtmlView.php
@@ -3,6 +3,7 @@ namespace Xdecaro\Component\Decarocourses\Administrator\View\Courses;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
@@ -14,21 +15,50 @@ class HtmlView extends BaseHtmlView
     public $items;
     public $pagination;
     public $state;
+    public $stats;
+    public bool $canCreate = false;
+    public bool $canEdit = false;
+    public bool $canEditState = false;
+    public bool $canDelete = false;
 
     public function display($tpl = null): void
     {
         UiHelper::loadAssets();
         HTMLHelper::_('behavior.multiselect');
+
         $this->items = $this->get('Items');
         $this->pagination = $this->get('Pagination');
         $this->state = $this->get('State');
+        $this->stats = $this->get('Stats');
+
+        $identity = Factory::getApplication()->getIdentity();
+        $this->canCreate = $identity->authorise('core.create', 'com_decarocourses');
+        $this->canEdit = $identity->authorise('core.edit', 'com_decarocourses');
+        $this->canEditState = $identity->authorise('core.edit.state', 'com_decarocourses');
+        $this->canDelete = $identity->authorise('core.delete', 'com_decarocourses');
 
         ToolbarHelper::title(Text::_('COM_DECAROCOURSES_COURSES'), 'stack');
-        ToolbarHelper::addNew('course.add');
-        ToolbarHelper::editList('course.edit');
-        ToolbarHelper::publish('courses.publish');
-        ToolbarHelper::unpublish('courses.unpublish');
-        ToolbarHelper::trash('courses.trash');
+
+        if ($this->canCreate) {
+            ToolbarHelper::addNew('course.add');
+        }
+
+        if ($this->canEdit) {
+            ToolbarHelper::editList('course.edit');
+        }
+
+        if ($this->canEditState) {
+            ToolbarHelper::publish('courses.publish');
+            ToolbarHelper::unpublish('courses.unpublish');
+
+            if ((string) $this->state->get('filter.state') !== '-2') {
+                ToolbarHelper::trash('courses.trash');
+            }
+        }
+
+        if ($this->canDelete && (string) $this->state->get('filter.state') === '-2') {
+            ToolbarHelper::deleteList('', 'courses.delete');
+        }
 
         parent::display($tpl);
     }
