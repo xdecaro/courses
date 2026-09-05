f2be2fa839d9b5fdcb62899fd1422df05e7fd7a9
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-05 23:53:10 +0200

    Respect ACL in course editor

--- component/admin/src/View/Course/HtmlView.php
+++ component/admin/src/View/Course/HtmlView.php
@@ -3,6 +3,7 @@ namespace Xdecaro\Component\Decarocourses\Administrator\View\Course;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
@@ -12,18 +13,38 @@ class HtmlView extends BaseHtmlView
 {
     public $form;
     public $item;
+    public bool $canSave = false;
+    public bool $canCreate = false;
 
     public function display($tpl = null): void
     {
         UiHelper::loadAssets();
+
         $this->form = $this->get('Form');
         $this->item = $this->get('Item');
 
         $isNew = empty($this->item->id);
-        ToolbarHelper::title($isNew ? Text::_('COM_DECAROCOURSES_COURSE_NEW') : Text::_('COM_DECAROCOURSES_COURSE_EDIT'), 'pencil-2');
-        ToolbarHelper::apply('course.apply');
-        ToolbarHelper::save('course.save');
-        ToolbarHelper::save2new('course.save2new');
+        $identity = Factory::getApplication()->getIdentity();
+
+        $this->canCreate = $identity->authorise('core.create', 'com_decarocourses');
+        $this->canSave = $isNew
+            ? $this->canCreate
+            : $identity->authorise('core.edit', 'com_decarocourses');
+
+        ToolbarHelper::title(
+            $isNew ? Text::_('COM_DECAROCOURSES_COURSE_NEW') : Text::_('COM_DECAROCOURSES_COURSE_EDIT'),
+            'pencil-2'
+        );
+
+        if ($this->canSave) {
+            ToolbarHelper::apply('course.apply');
+            ToolbarHelper::save('course.save');
+        }
+
+        if ($this->canCreate) {
+            ToolbarHelper::save2new('course.save2new');
+        }
+
         ToolbarHelper::cancel('course.cancel', 'JTOOLBAR_CLOSE');
 
         parent::display($tpl);
